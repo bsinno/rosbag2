56312de9a5043f9e4922b1d76b64c9e24fb28b6a
Alessandro Bottero <external.Alessandro.Bottero@bosch-si.com>
2018-11-27 11:09:37 +0100

    GH-38 Add storage_id option to CLI for play

--- ros2bag/ros2bag/verb/play.py
+++ ros2bag/ros2bag/verb/play.py
@@ -26,10 +26,13 @@ class PlayVerb(VerbExtension):
     def add_arguments(self, parser, cli_name):  # noqa: D102
         parser.add_argument(
             'bag_file', help='bag file to replay')
+        parser.add_argument(
+            '-s', '--storage', default='sqlite3',
+            help='storage identifier to be used, defaults to "sqlite3"')
 
     def main(self, *, args):  # noqa: D102
         bag_file = args.bag_file
         if not os.path.exists(bag_file):
             return "Error: bag file '{}' does not exist!".format(bag_file)
 
-        rosbag2_transport_py.play(uri=bag_file, storage_id='sqlite3')
+        rosbag2_transport_py.play(uri=bag_file, storage_id=args.storage)
